ff6abeb88a5e7c094ba5b5c94eb6986333516cb1
kino <66652533+kino-ona@users.noreply.github.com>
2021-07-12 17:38:39 +0900

    lgcampaign

---
+++ src/care-for-what-you-wear2021/js/clothing_campaign.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function(){
+
+	$('.clothing_keyvisual .over_text .item:not(".item_title")').each(function(i, e) {
+		var $Item = $(this);
+		var r = i + 1;
+
+		if($('.clothing_keyvisual .vodbg').find('video').length > 0) {
+			$Item.hover(function() {
+				$('.clothing_keyvisual').removeClass('origin');
+				$('.clothing_keyvisual').addClass('active');
+				$('.clothing_keyvisual .vodbg').find('video').get(0).pause();
+				$('.clothing_keyvisual .vodbg').find('video').stop().fadeOut(500);
+
+				$('.clothing_keyvisual .vodbg').find('.video0' + r).stop().fadeIn(500);
+				$('.clothing_keyvisual .vodbg').find('.video0' + r).get(0).play();
+			}, function() {
+				$('.clothing_keyvisual .vodbg').find('.video0' + r).stop().fadeIn(500);
+				$('.clothing_keyvisual .vodbg').find('.video0' + r).get(0).play();
+			});
+		}
+
+		$Item.each(function(){
+			$(this).focus(function(){
+				if($('.clothing_keyvisual .vodbg').find('video').length > 0){
+					$('.clothing_keyvisual .vodbg').find('video').get(0).pause();
+					$('.clothing_keyvisual .vodbg').find('video').stop().fadeOut(500);
+			
+					$('.clothing_keyvisual .vodbg').find('.video0' + r).stop().fadeIn(500);
+					$('.clothing_keyvisual .vodbg').find('.video0' + r).get(0).play();
+				}
+			});
+		})
+
+		$('.clothing_keyvisual').mouseleave(function(){
+			$('.clothing_keyvisual').removeClass('active');
+			$('.clothing_keyvisual').addClass('origin');
+			$('.clothing_keyvisual .vodbg').find('video').get(0).pause();
+			$('.clothing_keyvisual .vodbg').find('video').stop().fadeOut(500);
+		})
+	});
+
+
+});
